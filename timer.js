40d83778cfebf15222c28dab2b9691421d057362
Ckjr <conan.nma@gmail.com>
2016-09-10 16:53:16 +0800

    Add addsound, removesound, getsound to timer.js

---
+++ timer.js
@@ -0,0 +1,22 @@
+function addSound(sound, tick) {
+	var current_sounds = sounds_to_play[tick];  // Array of sounds to played at that particular tick
+	current_sounds.push(sound);
+	sounds_to_play[tick] = current_sounds;
+}
+
+function removeSound(sound, tick) {
+	var current_sounds = sounds_to_play[tick];
+	var foundIndex = current_sounds.indexOf(sound);
+	if (foundIndex > -1) {
+		current_sounds.splice(index, 1);
+		sounds_to_play[tick] = current_sounds;
+	} else {
+		window.alert("Warning: sound not found");
+	}
+}
+
+function getSound(path) {
+	return new Howl({
+		src: [path]
+	})
+}
