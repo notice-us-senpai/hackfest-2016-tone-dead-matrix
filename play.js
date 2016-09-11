7dabc488c0517cdf00ed957732d7f34d3e9c119d
leeyimin <imleeyimin@gmail.com>
2016-09-11 09:44:40 +0800

    Add Pause/Play button

--- play.js
+++ play.js
@@ -1,30 +1,28 @@
 function play() {
-	var sounds_at_curr_tick = sounds_to_play[tick];
+	if(toPlay){
+		var sounds_at_curr_tick = sounds_to_play[tick];
 
-	tick++;
-	tick%=16;
+		tick++;
+		tick%=16;
 
-	for (var k = 0; k < sounds_at_curr_tick; k++) {
-		console.log(sounds_at_curr_tick[k]);
-	}
-
-	if (sounds_at_curr_tick.length != 0) {
-		// No sound at this tick
-
-		var audio_at_curr_tick = [];
-
-		for (var i = 0; i < sounds_at_curr_tick.length; i++) {
-			audio_at_curr_tick[i] = new Audio(sounds_at_curr_tick[i]);
+		for (var k = 0; k < sounds_at_curr_tick; k++) {
+			console.log(sounds_at_curr_tick[k]);
 		}
 
-		for (var j = 0; j < sounds_at_curr_tick.length; j++) {
-			audio_at_curr_tick[j].play();
+		if (sounds_at_curr_tick.length != 0) {
+			// No sound at this tick
+
+			var audio_at_curr_tick = [];
+
+			for (var i = 0; i < sounds_at_curr_tick.length; i++) {
+				audio_at_curr_tick[i] = new Audio(sounds_at_curr_tick[i]);
+			}
+
+			for (var j = 0; j < sounds_at_curr_tick.length; j++) {
+				audio_at_curr_tick[j].play();
+			}
 		}
 	}
-
 	setTimeout(play,200);
-
-
-
 
 }
